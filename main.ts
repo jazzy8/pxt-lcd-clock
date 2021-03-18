680ae5b783df2f26f906b38ef9e99ddcd385b1a9
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-03-18 13:30:51 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    h += 1
-    if (h > 23) {
-        h = h - 24
-    }
-    display_the_time()
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.pause(5000)
-    if (input.buttonIsPressed(Button.AB)) {
-        control.reset()
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    s = 0
-    m += 1
-    if (m > 59) {
-        m = m - 60
-    }
-    display_the_time()
-})
 function display_the_time () {
     ampm = "am"
     qH = h
@@ -36,24 +15,200 @@ function display_the_time () {
     } else {
         qM = convertToText(m)
     }
-    Lcd.showText("" + qH + ":" + qM + " " + ampm + " ", 0, 0)
+    serial.writeLine("cls::all")
+    basic.pause(100)
+    serial.writeLine("shw::" + qH + ":" + qM + " " + ampm + " ")
 }
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P12, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
+    if (mde == 1) {
+        if (item == 0) {
+            mde = 2
+            serial.writeLine("cls::all")
+            serial.writeLine("shw::Set Hour")
+            serial.writeLine("cur::00,1")
+            serial.writeLine("shw::" + h)
+        } else if (item == 1) {
+            mde = 4
+            serial.writeLine("cls::all")
+            serial.writeLine("shw::Set Brightness")
+            serial.writeLine("cur::00,1")
+            serial.writeLine("shw::" + h)
+        } else if (item == 2) {
+            mde = 0
+            display_the_time()
+        }
+    } else if (mde == 2) {
+        mde = 3
+        serial.writeLine("cls::all")
+        serial.writeLine("shw::Set Minute")
+        serial.writeLine("cur::00,1")
+        serial.writeLine("shw::" + m)
+    } else if (mde == 3) {
+        mde = 0
+        display_the_time()
+    } else if (mde == 4) {
+        mde = 0
+        display_the_time()
+    }
+})
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P8, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
+    if (mde == 0) {
+        mde = 1
+        item = 0
+        serial.writeLine("cls:all")
+        basic.pause(100)
+        serial.writeLine("shw::Set Time")
+    } else {
+        mde = 0
+        display_the_time()
+    }
+})
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P1, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
+    if (mde == 0) {
+        item = item - 1
+        if (item < 0) {
+            item = 2
+        }
+        serial.writeLine("cls:all")
+        basic.pause(100)
+        if (item == 0) {
+            serial.writeLine("shw::Set Time")
+        } else if (item == 1) {
+            serial.writeLine("shw::Set Brightness")
+        } else if (item == 2) {
+            serial.writeLine("shw::Back")
+        }
+    } else if (mde == 2) {
+        h = h - 1
+        if (h < 0) {
+            h = 23
+        }
+        serial.writeLine("cls::all")
+        serial.writeLine("shw::Set Hour")
+        serial.writeLine("cur::00,1")
+        serial.writeLine("shw::" + h)
+    } else if (mde == 3) {
+        m = m - 1
+        s = 0
+        if (m < 0) {
+            m = 59
+        }
+        serial.writeLine("cls::all")
+        serial.writeLine("shw::Set Minute")
+        serial.writeLine("cur::00,1")
+        serial.writeLine("shw::" + m)
+    } else if (mde == 4) {
+        if (bright > 0) {
+            bright = bright - 1
+            serial.writeLine("cls::all")
+            serial.writeLine("shw::Set Brightness")
+            serial.writeLine("cur::00,1")
+            serial.writeLine("shw::" + bright)
+            serial.writeLine("bkl::" + bright)
+        }
+    }
+})
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P2, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
+    if (mde == 0) {
+        item = item + 1
+        if (item > 2) {
+            item = 0
+        }
+        serial.writeLine("cls:all")
+        basic.pause(100)
+        if (item == 0) {
+            serial.writeLine("shw::Set Time")
+        } else if (item == 1) {
+            serial.writeLine("shw::Set Brightness")
+        } else if (item == 2) {
+            serial.writeLine("shw::Back")
+        }
+    } else if (mde == 2) {
+        h = h + 1
+        if (h > 23) {
+            h = 0
+        }
+        serial.writeLine("cls::all")
+        serial.writeLine("shw::Set Hour")
+        serial.writeLine("cur::00,1")
+        serial.writeLine("shw::" + h)
+    } else if (mde == 3) {
+        m = m + 1
+        if (m > 59) {
+            m = 0
+        }
+        serial.writeLine("cls::all")
+        serial.writeLine("shw::Set Minute")
+        serial.writeLine("cur::00,1")
+        serial.writeLine("shw::" + m)
+    } else if (mde == 4) {
+        if (bright < 255) {
+            bright = bright + 1
+            serial.writeLine("cls::all")
+            serial.writeLine("shw::Set Brightness")
+            serial.writeLine("cur::00,1")
+            serial.writeLine("shw::" + bright)
+            serial.writeLine("bkl::" + bright)
+        }
+    }
+})
 let diff = 0
 let new_ms = 0
 let qM = ""
 let qH = 0
 let ampm = ""
+let mde = 0
 let s = 0
 let m = 0
 let h = 0
-Lcd.initDisplay()
+let item = 0
+let bright = 0
 led.enable(false)
+bright = 255
+item = 0
 h = 0
 m = 0
 s = 0
-let old_ms = 0
-let mde = 0
+mde = 0
+serial.redirect(
+SerialPin.P14,
+SerialPin.P15,
+BaudRate.BaudRate9600
+)
+basic.pause(100)
+serial.writeLine("shw::BBC Microbit")
+basic.pause(100)
+serial.writeLine("bkl::255")
+basic.pause(500)
+display_the_time()
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+        control.raiseEvent(
+        EventBusSource.MICROBIT_ID_IO_P8,
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        )
+    }
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        control.raiseEvent(
+        EventBusSource.MICROBIT_ID_IO_P1,
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        )
+    }
+    if (pins.digitalReadPin(DigitalPin.P2) == 0) {
+        control.raiseEvent(
+        EventBusSource.MICROBIT_ID_IO_P2,
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        )
+    }
+    if (pins.digitalReadPin(DigitalPin.P12) == 0) {
+        control.raiseEvent(
+        EventBusSource.MICROBIT_ID_IO_P12,
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        )
+    }
+})
 control.inBackground(function () {
+    let old_ms = 0
     new_ms = input.runningTime()
     diff = new_ms - old_ms
     if (diff >= 1000) {
